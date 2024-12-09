8af0e73fab2d766c818ecc4c44076844e5aefc66
Aminam78 <110155175+Aminam78@users.noreply.github.com>
2024-12-09 14:17:07 +0330

    Added: GraphQL API Settings
    Added apollo client js and edited main.jsx, and package.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import "./index.css"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ApolloProvider } from '@apollo/client';
+import client from './apolloClient';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ApolloProvider client={client}>
     <App />
-  </StrictMode>,
-)
+  </ApolloProvider>
+);
