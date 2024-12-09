860c726e6114c91c7f7549c344cd3dbef6440c7f
Aminam78 <110155175+Aminam78@users.noreply.github.com>
2024-12-09 18:05:00 +0330

    Added: icons and pic for EventCard

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,15 +2,10 @@ import React from "react";
 import HeroSection from '../../components/events/herosection/HeroSection'
 
 const HomePage = () => {
-  const events = [
-    { id: 1, image: "/picture-1.png", title: "رویداد ۱", time: "۱۰:۳۰ دوشنبه", participants: "۲۳ نفر", location: "مکان ۱", category: "دسته‌بندی ۱" },
-    { id: 2, image: "/picture-2.png", title: "رویداد ۲", time: "۱۱:۳۰ سه‌شنبه", participants: "۱۲ نفر", location: "مکان ۲", category: "دسته‌بندی ۲" },
-    { id: 3, image: "/picture-3.png", title: "رویداد ۳", time: "۱۲:۳۰ چهارشنبه", participants: "۳۴ نفر", location: "مکان ۳", category: "دسته‌بندی ۳" },
-    { id: 4, image: "/picture-4.png", title: "رویداد ۴", time: "۱۴:۳۰ پنج‌شنبه", participants: "۴۵ نفر", location: "مکان ۴", category: "دسته‌بندی ۴" },
-  ];
+
   return (
     <>
-    <HeroSection/>
+    <HeroSection/> 
     </>
   );
 };
