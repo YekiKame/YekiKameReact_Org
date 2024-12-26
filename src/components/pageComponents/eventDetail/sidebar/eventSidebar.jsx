b69a5d46cce0dcde6adb17357f821000c7dcee78
Erfan79Eftekhari <85516600+Erfan79Eftekhari@users.noreply.github.com>
2024-12-26 21:32:24 +0330

    Delete src directory

--- src/components/pageComponents/eventDetail/sidebar/eventSidebar.jsx
+++
@@ -1,43 +0,0 @@
-import React from "react";
-import "./eventsideBar.module.css";
-import sportIcon from "../../../../assets/icons/sport.svg";
-import TimerIcon from "../../../../assets/icons/timer.svg";
-
-const SideBar = () => {
-  return (
-    <div className="event-card">
-      <h1 className="event-title">بازی تنیس</h1>
-
-      <div className="event-details">
-        <div className="event-detail">
-          <span className="event-icon">
-            <img src={sportIcon}></img>
-          </span>
-          <span className="event-text">ورزشی</span>
-        </div>
-
-        <div className="event-detail">
-          <span className="event-icon">
-            <img src={TimerIcon}></img>
-          </span>
-          <span className="event-text">دوشنبه ۲۲ بهمن ۱۴۰۳ - ساعت ۲۲:۳۰</span>
-        </div>
-        {/* <a href="#" className="add-to-calendar">
-          اضافه به تقویم
-        </a> */}
-
-        <div className="event-detail">
-          <span className="event-icon">📍</span>
-          <span className="event-text">
-            تهران میدان رسالت خیابان هنگام خیابان دانشگاه علم و صنعت پلاک ۳۵
-            واحد ۳
-          </span>
-        </div>
-      </div>
-
-      <button className="event-button">شرکت در رویداد</button>
-    </div>
-  );
-};
-
-export default SideBar;
--- src/components/userdashboard/myevents/MyEventsTab.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MyEventsTab = () => {
-  return (
-    <div>MyEventsTab</div>
-  )
-}
-
-export default MyEventsTab
