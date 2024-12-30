7583d63d318091cdc2bba799743964bdff0e9f81
Erfan <erfaneftekhari0079@gmail.com>
2024-12-30 08:31:18 +0330

    working on event list page.

--- src/components/events/eventlist/eventListEventCard.jsx
+++ src/components/events/eventlist/eventListEventCard.jsx
@@ -3,11 +3,14 @@ import PropTypes from "prop-types";
 import Button from "../../shared/button/button.jsx";
 import styles from "./eventlisteventcard.module.css";
 
-// آیکون‌ها
 import TimerIcon from "../../../assets/icons/timer.svg";
 import UserIcon from "../../../assets/icons/user2.svg";
 import LocationIcon from "../../../assets/icons/location2.svg";
 import LeadingIcon from "../../../assets/icons/leading-icon.svg";
+import leisureIcon from "../../../assets/icons/entertainment.svg";
+import sportIcon from "../../../assets/icons/sport.svg";
+import cultureIcon from "../../../assets/icons/art.svg";
+import educationIcon from "../../../assets/icons/education.svg";
 
 const EventListEventCard = ({
   title,
@@ -17,6 +20,12 @@ const EventListEventCard = ({
   neighborhood,
   image,
 }) => {
+  const categoryIcons = {
+    تفریحی: leisureIcon,
+    ورزشی: sportIcon,
+    فرهنگی: cultureIcon,
+    آموزشی: educationIcon,
+  };
   // فرمت کردن تاریخ و زمان
   const eventDate = new Date(startDate);
   const formattedDate = eventDate.toLocaleDateString("fa-IR", {
@@ -26,12 +35,38 @@ const EventListEventCard = ({
     hour: "2-digit",
     minute: "2-digit",
   });
-
   return (
     <div className={styles["event-card"]}>
       <div className={styles["event-info"]}>
-        <div className="event-info-text"></div>
-        <div className="event-info-image"></div>
+        <div className={styles["event-info-text"]}>
+          <div className={styles["enent-card-title"]}>{title}</div>
+          <div className={styles["eventTime"]}>
+            <img src={TimerIcon} alt="زمان" className={styles["icon"]} />
+            <span>{`${formattedDate} ساعت ${formattedTime}`}</span>
+          </div>
+          <div className={styles["infoItem"]}>
+            <img
+              src={categoryIcons[eventCategory]} // انتخاب آیکون بر اساس دسته‌بندی
+              alt={eventCategory}
+              className={styles["icon"]}
+            />
+            <div>{eventCategory}</div>
+          </div>
+          <div className={styles["event-card-info-subinfo"]}>
+            <div className={styles["infoItem"]}>
+              <img src={LocationIcon} alt="مکان" className={styles["icon"]} />
+              <span>{neighborhood}</span>
+            </div>
+            <div className={styles["infoItem"]}>
+              <img src={UserIcon} alt="اعضا" className={styles["icon"]} />
+              <span>{`${subscriberCount} نفر`}</span>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles["event-info-image"]}>
+          <img src={image} alt="Event" className={styles["image"]} />
+        </div>
       </div>
       <Button
         text={"عضو شدن"}
@@ -50,10 +85,7 @@ const EventListEventCard = ({
 
 EventListEventCard.propTypes = {
   title: PropTypes.string.isRequired,
-  eventCategory: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-  }).isRequired,
+  eventCategory: PropTypes.string.isRequired,
   subscriberCount: PropTypes.number.isRequired,
   startDate: PropTypes.string.isRequired,
   neighborhood: PropTypes.string.isRequired,
